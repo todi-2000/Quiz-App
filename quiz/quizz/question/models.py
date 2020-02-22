fc1e2fb6e010d5ab80efcef9267642126237afd1
todi-2000 <manshitodi@gmail.com>
2020-02-23 01:42:13 +0530

    Change in Template

--- quiz/quizz/question/models.py
+++ quiz/quizz/question/models.py
@@ -3,11 +3,15 @@ from django.db import models
 # Create your models here.
 class Question(models.Model):
     question_text=models.CharField(max_length=600)
+    max_marks=models.DecimalField(default=0,decimal_places=2,max_digits=6)
 
     def __str__(self):
         return self.question_text
 
-class Answer(models.Model):
+class Choice(models.Model):
     question=models.ForeignKey(Question,on_delete=models.CASCADE)
     choice=models.CharField(max_length=500)
-    points=models.IntegerField(default=10)
+    answer=models.BooleanField(max_length=2,default=False,null=False)
+
+    def __str__(self):
+        return self.choice
--- quiz/quizz/question/views.py
+++ quiz/quizz/question/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Question,Answer
+from django.shortcuts import render,get_object_or_404
+from .models import Question,Choice
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -8,8 +9,9 @@ def home(request):
 def rules(request):
     return render(request,'question/rules.html')
 
+@login_required
 def details(request,question_id):
-    question=Question.objects.all()
+    question=Question.objects.get(pk=question_id)
     context={
         'question':question
     }
