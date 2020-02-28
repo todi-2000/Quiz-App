509cc7326b1a9a52c445f70e35049e8594c61224
todi-2000 <manshitodi@gmail.com>
2020-02-28 16:41:21 +0530

    Changes in views

--- quiz/quizz/question/views.py
+++ quiz/quizz/question/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Question,Choice
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -13,8 +14,12 @@ def rules(request):
 def details(request,question_id):
     question=Question.objects.get(pk=question_id)
     choice=Choice.objects.get(question=question.id)
+    if request.method=='POST':
+        if request.POST.get('ans',default=None) == choice:
+            messages.success(request,f'Correct Answer')
+            print('correct')
     context={
-        'question':question,
-        'choice':choice
+        'question':question
     }
+
     return render(request,'question/details.html',context)
